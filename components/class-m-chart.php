c9e185f11b8261ce907154422aa1e3f1b7c7ccd6
Jamie Poitra <jamie@methnen.com>
2022-02-03 17:00:34 -0800

    Updating version number stuff

--- components/class-m-chart.php
+++ components/class-m-chart.php
@@ -2,7 +2,7 @@
 
 class M_Chart {
 	public $dev = true;
-	public $version = '1.8.1';
+	public $version = '1.9';
 	public $slug = 'm-chart';
 	public $plugin_name = 'Chart';
 	public $chart_meta_fields = array(
