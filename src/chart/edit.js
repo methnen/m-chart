fa53401ba03bb5574b1a6135a4dc426ba827fc8e
Inigo I.J.C.Grimbergen <ijc.grimbergen@belastingdienst.nl>
2023-07-06 14:27:46 +0200

    -raised number of scripts to be fetched -simplyfied the error states & retrieve flow

--- src/chart/edit.js
+++ src/chart/edit.js
@@ -9,9 +9,9 @@ import "./editor.scss";
 export default function edit( { attributes, setAttributes } ) {
 
     const [ options, setOptions ] = useState( [] );
-    const [ id, setId ] = useState( '' );
+    const [ search, setSearch ] = useState( '' );
+    const [ loaded, setLoaded ] = useState( false );
     const [ charts, setCharts ] = useState( [] );
-    const [ chartsAvailable, setChartsAvailable ] = useState( true );
     const [ pluginActive, setPluginActive ] = useState( true );
     const blockProps = useBlockProps( { className: 'm-chart-block-chart-selector' } );
 
@@ -19,28 +19,13 @@ export default function edit( { attributes, setAttributes } ) {
     // We then check if a chart is already chosen. If so we show that one else show all.
 
     useEffect( () => {
-        apiFetch( { path: '/wp/v2/m-chart?per_page=100&status=publish&_fields=id,title,subtitle,url' } ).then( result => {
-            setChartsAvailable( true );
+        apiFetch( { path: '/wp/v2/m-chart?all_charts&status=publish&_fields=id,title,subtitle,url' } ).then( result => {
+
             let charts = [];
             result.map( x => charts.push( { id: x.id, title: x.title || '-', subtitle: x.subtitle, src: x.url || defaultImg } ) );
             setCharts( charts );
-            // Any charts?
-            if ( charts.length === 0 ) {
-                setChartsAvailable( false );
-            } else {
-                // Store the charts.
-                setOptions( charts );
-                // Is one chosen?
-                if ( attributes.chartId ) {
-                    const idAvailable = charts.some( x => x.id === attributes.chartId );
-                    if ( idAvailable ) {
-                        setId( attributes.chartId );
-                    } else {
-                        setId( '' );
-                        setAttributes( { chartId: '', chartTitle: '', title: '' } );
-                    }
-                }
-            }
+            setOptions( charts );
+            setLoaded( true );
         } ).catch( ( error ) => {
             if ( error.code === 'rest_no_route' ) {
                 setPluginActive( false );
@@ -54,19 +39,18 @@ export default function edit( { attributes, setAttributes } ) {
     } );
 
     //
-    const selected = options.filter( x => x.id === id )[ 0 ];
+    const selected = charts.filter( x => x.id === attributes.chartId )[ 0 ];
 
     const handleClick = ( id ) => {
-        setId( id );
-        let title = options.filter( x => x.id === id )[ 0 ]?.title;
-        setAttributes( { chartId: id, chartTitle: title, title: '' } );
+        setAttributes( { chartId: id } );
     };
 
     const handleFilter = ( value ) => {
-        setAttributes( { title: value } );
+
+        setSearch( value );
         if ( value?.length > 1 ) {
-            setId( null );
-            setOptions( options.filter( x => x.title.includes( value ) || x.subtitle.includes( value ) ) );
+            attributes.chartId = null;
+            setOptions( charts.filter( x => x.title.includes( value ) || x.subtitle.includes( value ) ) );
         }
         if ( value === '' ) {
             setOptions( charts );
@@ -77,42 +61,51 @@ export default function edit( { attributes, setAttributes } ) {
         <div { ...blockProps }>
             <div className="wp-block m-chart-selector-container">
                 <TextControl
-                    value={ attributes.title }
+                    value={ search }
                     placeholder={ __( 'Search by title or subtitle', 'm-chart' ) }
                     onChange={ ( value ) => handleFilter( value ) }
                 />
+
                 { optionsList.length > 0 &&
-
                     <div className="m-chart-block-instructions">
-                        { !id && <span>{ __( 'Click on a chart to select & insert', 'm-chart' ) }</span> }
-                        { id && <span>{ __( 'Click on the chart to make a new selection', 'm-chart' ) }</span> }
+                        { !attributes.chartId && <span>{ __( 'Click on a chart to select & insert', 'm-chart' ) }</span> }
+                        { attributes.chartId && <span>{ __( 'Click on the chart to make a new selection', 'm-chart' ) }</span> }
                     </div>
                 }
 
-                { chartsAvailable === true && pluginActive === true && optionsList.length === 0 &&
+                { pluginActive === false &&
                     <div className="m-chart-centralizer">
-                        <Spinner />
+                        <p>{ __( 'Chart plugin not available or not active', 'm-chart' ) }
+                            <a href="/wp-admin/plugins.php">{ __( 'to plugins page', 'm-chart' ) }</a>
+                        </p>
                     </div>
                 }
 
-                { ( chartsAvailable === false || pluginActive === false ) &&
+                { pluginActive === true &&
                     <div className="m-chart-centralizer">
-                        { chartsAvailable === false &&
+                        { !loaded &&
+                            <Spinner />
+                        }
+                        { loaded && charts.length === 0 &&
                             <p>{ __( 'no charts found', 'm-chart' ) }
-                                <a href="/wp-admin/admin.php?page=visualizer">{ __( 'create a new chart', 'm-chart' ) }</a></p> }
-                        { pluginActive === false &&
-                            <p>Chart plugin not available or not active
-                                <a href="/wp-admin/plugins.php">{ __( 'to plugins page', 'm-chart' ) }</a></p> }
+                                <a href="/wp-admin/post-new.php?post_type=m-chart">{ __( 'create a new chart', 'm-chart' ) }</a>
+                            </p>
+                        }
+                        {
+                            loaded && charts.length > 0 && optionsList.length === 0 && search.length > 1 &&
+                            <p>{ __( 'no charts found using this search string', 'm-chart' ) }</p>
+                        }
                     </div>
                 }
 
-                { !id &&
+
+                { !attributes.chartId &&
                     <ul className="m-chart-containerlist">
                         { optionsList }
                     </ul>
                 }
 
-                { id &&
+                { attributes.chartId &&
                     <div className="selected-chart" onClick={ () => handleClick( '' ) }>
                         <img className={ selected?.src.includes( 'icon-256x256' ) ? 'default-img' : 'preview' } src={ selected?.src } />
                     </div>
