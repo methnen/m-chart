3d75dc4926a2cf4a3419ccb6179eb6717bd31ec4
Inigo I.J.C.Grimbergen <ijc.grimbergen@belastingdienst.nl>
2023-07-06 14:27:46 +0200

    -raised number of scripts to be fetched -simplyfied the error states & retrieve flow

--- components/class-m-chart-block.php
+++ components/class-m-chart-block.php
@@ -13,6 +13,16 @@ class M_Chart_Block {
 		$this->build_folder_path = plugin_dir_path( __DIR__ ) . 'build/';
 		add_action( 'init', array( $this, 'register_m_chart_block_support' ) );
 		add_filter( 'rest_prepare_m-chart', array( $this, 'filter_m_chart_json' ), 10, 3 );
+		add_filter( 'rest_m-chart_query', array( $this, 'set_custom_max_post_per_page' ), 10, 2 );
+	}
+
+
+
+	function set_custom_max_post_per_page( $args, $request ) {
+		// Default only 100 items per request can be fetcht, we set it to 1000
+		$max                    = max( (int) $request->get_param( 'all_charts' ), 1000 );
+		$args['posts_per_page'] = $max;
+		return $args;
 	}
 
 	public function register_m_chart_block_support() {
@@ -46,16 +56,18 @@ class M_Chart_Block {
 
 	}
 
+	// Morph the api request response to fit our needs: e.g. a preview url and subtitle to search through.
 	public function filter_m_chart_json( $data, $post, $context ) {
 		$post_meta              = get_post_meta( $post->ID, 'm-chart', true );
 		$data->data['id']       = strval( $data->data['id'] );
 		$data->data['title']    = strtolower( $data->data['title']['rendered'] ) ?? '';
 		$data->data['url']      = get_the_post_thumbnail_url( $post->ID );
-		$data->data['subtitle'] = strtolower( $post_meta['subtitle'] ) ?? '';
+		$data->data['subtitle'] = isset( $post_meta ) && isset( $post_meta['subtitle'] ) ? strtolower( $post_meta['subtitle'] ) : '';
 
 		return $data;
 	}
 
+	// Create a version string to add to the loaded script & style files, but refresh if in develop mode.
 	public function version_str() {
 		$plugin_file    = plugin_dir_path( __DIR__ ) . ( 'm-chart.php' );
 		$plugin_data    = get_file_data( $plugin_file, array( 'Version' => 'Version' ) );
