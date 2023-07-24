ea3c20680a3868657d6338cbf3c211cb4620aa4e
Inigo I.J.C.Grimbergen <ijc.grimbergen@belastingdienst.nl>
2023-07-24 13:00:06 +0200

    improved user experience, support for setting: no images

--- components/class-m-chart-block.php
+++ components/class-m-chart-block.php
@@ -12,6 +12,8 @@ class M_Chart_Block {
 		$this->build_folder_url  = plugin_dir_url( __DIR__ ) . 'build/';
 		$this->build_folder_path = plugin_dir_path( __DIR__ ) . 'build/';
 		add_action( 'init', array( $this, 'register_m_chart_block_support' ) );
+		add_action( 'plugins_loaded', array( $this, 'wt_m_chart_load_textdomain' ) );
+		add_action( 'rest_api_init', array( $this, 'register_fetch_m_chart_options' ) );
 		add_filter( 'rest_prepare_m-chart', array( $this, 'filter_m_chart_json' ), 10, 3 );
 		add_filter( 'rest_m-chart_query', array( $this, 'set_custom_max_post_per_page' ), 10, 2 );
 	}
@@ -57,7 +59,7 @@ class M_Chart_Block {
 			$this->version_str()
 		);
 
-		// Registereditor styles.
+		// Register editor styles.
 		wp_register_style(
 			'm-chart-editor-style',
 			$this->build_folder_url . 'index.css',
@@ -70,14 +72,14 @@ class M_Chart_Block {
 	}
 
 	/**
-	 * Morph the api request response to fit our needs: e.g. a preview url and subtitle to search through.
+	 * Morph the api request response to fit our needs: e.g. a preview url and subtitle to show.
 	 */
 	public function filter_m_chart_json( $data, $post, $context ) {
 		$post_meta              = get_post_meta( $post->ID, 'm-chart', true );
 		$data->data['id']       = strval( $data->data['id'] );
-		$data->data['title']    = strtolower( $data->data['title']['rendered'] ) ?? '';
+		$data->data['title']    = $data->data['title']['rendered'] ?? '';
 		$data->data['url']      = get_the_post_thumbnail_url( $post->ID );
-		$data->data['subtitle'] = isset( $post_meta ) && isset( $post_meta['subtitle'] ) ? strtolower( $post_meta['subtitle'] ) : '';
+		$data->data['subtitle'] = isset( $post_meta ) && isset( $post_meta['subtitle'] ) ? $post_meta['subtitle'] : '';
 
 		return $data;
 	}
@@ -90,5 +92,35 @@ class M_Chart_Block {
 		$plugin_data    = get_file_data( $plugin_file, array( 'Version' => 'Version' ) );
 		$plugin_version = $plugin_data['Version'];
 		return WP_DEBUG ? $plugin_version . ' - ' . substr( hash( 'sha256', current_time( 'timestamp' ) ), 0, 12 ) : $plugin_version;
+	}
+
+	/**
+	* Load the plugin's text domain
+	*/
+	public function wt_m_chart_load_textdomain() {
+		load_plugin_textdomain( 'm-chart', false, basename( __DIR__ ) . '/languages' );
+	}
+
+
+
+	public function register_fetch_m_chart_options() {
+			register_rest_route(
+				'm-chart/v1',
+				'/mchart_options/',
+				array(
+					'methods'             => 'GET',
+					'callback'            => array( $this, 'image_support_active' ),
+					'permission_callback' => function () {
+						return true;
+						return current_user_can( 'administrator' );
+					},
+				)
+			);
+	}
+	public function image_support_active() {
+		return array(
+			'siteurl'              => get_option( 'siteurl' ),
+			'image_support_active' => get_option( 'm-chart' )['performance'] === 'default',
+		);
 	}
 }
--- src/chart/edit.js
+++ src/chart/edit.js
@@ -1,118 +1,147 @@
-import { TextControl, Spinner } from '@wordpress/components';
+import { TextControl, Spinner, ToolbarGroup, ToolbarButton, } from '@wordpress/components';
 import { useState, useEffect } from '@wordpress/element';
-import { useBlockProps } from '@wordpress/block-editor';
+import { useBlockProps, BlockControls } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
 import apiFetch from '@wordpress/api-fetch';
-import defaultImg from '../../assets/icon-256x256.png';
 import "./editor.scss";
 
 export default function edit( { attributes, setAttributes } ) {
 
+    // State.
     const [ options, setOptions ] = useState( [] );
     const [ search, setSearch ] = useState( '' );
     const [ loaded, setLoaded ] = useState( false );
     const [ charts, setCharts ] = useState( [] );
-    const [ pluginActive, setPluginActive ] = useState( true );
+    const [ temp, setTemp ] = useState( null );
+    const [ siteUrl, setSiteUrl ] = useState( null );
+    const [ imageSupport, setImageSupport ] = useState( true );
+    const [ loadProblem, setLoadProblem ] = useState( false );
+    // Url constants.
+    const newUrl = `${ siteUrl }/wp-admin/post-new.php?post_type=m-chart`;
+    const editUrl = `${ siteUrl }/wp-admin/post.php?post=${ attributes.chartId }&action=edit`;
+    const imageSupportUrl = `/m-chart/v1/mchart_options`;
+    const chartFetchUrl = `/wp/v2/m-chart?all_charts&status=publish&_fields=id,title,subtitle,url`;
+    // Blockprops.
     const blockProps = useBlockProps( { className: 'm-chart-block-chart-selector' } );
 
     // On load we fetch all charts if none available we set a constant to show an error message.
     // We then check if a chart is already chosen. If so we show that one else show all.
-
     useEffect( () => {
-        apiFetch( { path: '/wp/v2/m-chart?all_charts&status=publish&_fields=id,title,subtitle,url' } ).then( result => {
+        apiFetch( { path: imageSupportUrl } ).then( result => {
+            setImageSupport( result.image_support_active );
+            setSiteUrl( result.siteurl );
+        } );
 
+        apiFetch( { path: chartFetchUrl } ).then( result => {
             let charts = [];
-            result.map( x => charts.push( { id: x.id, title: x.title || '-', subtitle: x.subtitle, src: x.url || defaultImg } ) );
+            result.map( x => charts.push( { id: x.id, title: x.title || '-', subtitle: x.subtitle, src: x.url || '' } ) );
             setCharts( charts );
             setOptions( charts );
             setLoaded( true );
         } ).catch( ( error ) => {
             if ( error.code === 'rest_no_route' ) {
-                setPluginActive( false );
+                setLoadProblem( true );
             };
         } );
     }, [] );
 
     // Compose preview list.
     const optionsList = options.map( ( x ) => {
-        return <li key={ x.id } onClick={ () => handleClick( x.id ) }><img className={ x.src.includes( 'icon-256x256' ) ? 'default-img' : 'preview' } src={ x.src } /></li>;
+        if ( imageSupport ) {
+            let random = `?random=${ Math.round( Math.random() * ( 999999, 100000 ) ) }`;
+            return <li className={ x.src ? 'img' : 'dashicons dashicons-chart-pie' } key={ x.id } onClick={ () => handleClick( x.id ) }>{ x.src ? <img src={ x.src + random } /> : <h6>{ x.title }</h6> }</li>;
+        } else {
+            return <li key={ x.id } onClick={ () => handleClick( x.id ) }> { x.title }</li>;
+        }
     } );
 
-    //
     const selected = charts.filter( x => x.id === attributes.chartId )[ 0 ];
 
     const handleClick = ( id ) => {
         setAttributes( { chartId: id } );
+        setTemp( id );
     };
 
     const handleFilter = ( value ) => {
-
         setSearch( value );
-        if ( value?.length > 1 ) {
+        if ( value.length === 0 ) {
+            if ( temp ) { setAttributes( { chartId: temp } ); }
+            else { setOptions( charts ); }
+        } else {
+            if ( attributes.chartId ) { setTemp( attributes.chartId ); }
             attributes.chartId = null;
-            setOptions( charts.filter( x => x.title.includes( value ) || x.subtitle.includes( value ) ) );
-        }
-        if ( value === '' ) {
-            setOptions( charts );
+            setOptions( charts.filter( x => x.title.toLowerCase().includes( value.toLowerCase() ) ) );
         }
     };
 
     return (
         <div { ...blockProps }>
+            <BlockControls>
+                <ToolbarGroup>
+                    <ToolbarButton onClick={ () => window.location.href = newUrl } icon="external">{ __( 'New Chart', 'wt-m-chart-addon' ) }</ToolbarButton>
+                    { attributes.chartId &&
+                        <>
+                            <ToolbarButton onClick={ () => window.location.href = editUrl } icon="external" >{ __( 'Edit Chart', 'wt-m-chart-addon' ) }</ToolbarButton>
+                            <ToolbarButton onClick={ () => handleClick( '' ) } >{ __( 'Replace', 'wt-m-chart-addon' ) }</ToolbarButton>
+                        </>
+                    }
+                </ToolbarGroup>
+            </BlockControls>
+
             <div className="wp-block m-chart-selector-container">
-                { optionsList.length > 0 &&
-                    <>
-                        <TextControl
-                            value={ search }
-                            placeholder={ __( 'Search by title or subtitle', 'm-chart' ) }
-                            onChange={ ( value ) => handleFilter( value ) }
-                        />
-                        <div className="m-chart-block-instructions">
-                            { attributes.chartId ?
-                                <span>{ __( 'Click on the chart to make a new selection', 'm-chart' ) }</span>
-                                :
-                                <span>{ __( 'Click on a chart to select & insert', 'm-chart' ) }</span>
-                            }
-                        </div>
-                    </>
-                }
-
-                { pluginActive ?
-                    <div className="m-chart-centralizer">
-                        { loaded ?
-                            charts.length === 0 ?
-                                <p>{ __( 'no charts found', 'm-chart' ) }
-                                    <a href="/wp-admin/post-new.php?post_type=m-chart">{ __( 'create a new chart', 'm-chart' ) }</a>
+                <h6 className="branding"><span className="dashicons dashicons-chart-pie"></span>M Chart</h6>
+                <div className="m-chart-viewbox">
+                    { loadProblem ?
+                        <p>{ __( 'There is a problem fetching charts', 'wt-m-chart-addon' ) }</p>
+                        :
+                        <>
+                            { !loaded ?
+                                <p className="center">
+                                    <Spinner />
                                 </p>
-
                                 :
-                                optionsList.length === 0 && search.length > 1 ?
-                                    <p>{ __( 'no charts found using this search string', 'm-chart' ) }</p>
+                                charts.length === 0 ?
+                                    <p className="center">{ __( 'no charts found', 'wt-m-chart-addon' ) }
+                                        <a href={ newUrl }>{ __( 'create a new chart', 'wt-m-chart-addon' ) }</a>
+                                    </p>
                                     :
-                                    null
-                            :
-                            <Spinner />
-                        }
-                    </div>
-                    :
-                    <div className="m-chart-centralizer">
-                        <p>{ __( 'Chart plugin not available or not active', 'm-chart' ) }
-                            <a href="/wp-admin/plugins.php">{ __( 'to plugins page', 'm-chart' ) }</a>
-                        </p>
-                    </div>
-                }
-
-
-                { attributes.chartId ?
-                    <div className="selected-chart" onClick={ () => handleClick( '' ) }>
-                        <img className={ selected?.src.includes( 'icon-256x256' ) ? 'default-img' : 'preview' } src={ selected?.src } />
-                    </div>
-                    :
-                    <ul className="m-chart-containerlist">
-                        { optionsList }
-                    </ul>
-                }
+                                    attributes.chartId ?
+                                        <div className="selected-chart">
+                                            { imageSupport ?
+                                                <>
+                                                    { selected?.src === '' ?
+                                                        <h4 className="no-title dashicons-before dashicons-chart-pie">{ selected?.title }</h4>
+                                                        :
+                                                        <img className="preview" src={ selected?.src } />
+                                                    }
+                                                </>
+                                                :
+                                                <>
+                                                    <h4>{ selected?.title }</h4>
+                                                    <p>{ selected?.subtitle }</p>
+                                                </>
+                                            }
+                                        </div>
+                                        :
+                                        <>
+                                            <TextControl
+                                                value={ search }
+                                                placeholder={ __( 'Search by title', 'wt-m-chart-addon' ) }
+                                                onChange={ ( value ) => handleFilter( value ) }
+                                            />
+                                            { optionsList.length === 0 && search.length > 1 ?
+                                                <p className="center">{ __( 'no charts found using this search string', 'wt-m-chart-addon' ) }</p>
+                                                :
+                                                <ul className={ `m-chart-containerlist ${ imageSupport ? 'image-support' : '' }` }>
+                                                    { optionsList }
+                                                </ul>
+                                            }
+                                        </>
+                            }
+                        </>
+                    }
+                </div>
             </div>
-        </div>
+        </div >
     );
 }
