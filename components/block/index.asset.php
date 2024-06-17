5ae57597486bf39ca49e26285d9b70c02b0c5be4
Inigo I.J.C.Grimbergen <ijc.grimbergen@belastingdienst.nl>
2024-06-17 15:42:13 +0200

    Dutch translations working, added formal Dutch translations

--- components/block/index.asset.php
+++ components/block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9a81147d426a677341b7');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '4df624cb7117aa8ddecf');
