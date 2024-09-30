fd29255253805325af52def5cf2a8a6bcd4ba14e
Jamie Poitra <jamie@methnen.com>
2024-09-30 07:56:45 -0700

    Removing debug code.

--- components/class-m-chart-chartjs.php
+++ components/class-m-chart-chartjs.php
@@ -167,7 +167,6 @@ class M_Chart_Chartjs {
 
 		// Run the parse class on the data
 		$data = isset( $this->post_meta['data']['sets'][0] ) ? $this->post_meta['data']['sets'][0] : '';
-		$data = '';
 		m_chart()->parse()->parse_data( $data, $this->post_meta['parse_in'] );
 
 		$type = $this->post_meta['type'];
