8fe583e3383a708e4442d045d07ad617d81e30c9
Jamie Poitra <jamie@methnen.com>
2026-03-06 16:41:42 -0800

    Added Copy to clipboard button to shortcode input.

--- components/admin-ui/index.asset.php
+++ components/admin-ui/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-element', 'wp-i18n'), 'version' => '718f3d34aef4d6f57730');
+<?php return array('dependencies' => array('react', 'wp-element', 'wp-i18n'), 'version' => 'b5edeca76e187b9a4f2c');
