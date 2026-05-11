499ef0476e6accd8442f52475547c527875c8eee
Jamie Poitra <jamie@methnen.com>
2026-05-11 10:34:16 -0700

    Sass fix.

--- components/block/index.asset.php
+++ components/block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '4655b998045983ded1a6');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9ed1b3053b1bcf9b029a');
