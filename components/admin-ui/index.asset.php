2567e19a3abaf7d9fa27b704e6c99f6ce417d696
Jamie Poitra <jamie@methnen.com>
2026-06-30 12:10:49 -0700

    New build after pulling updated dependencies.

--- components/admin-ui/index.asset.php
+++ components/admin-ui/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-a11y', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '577635a06c33c165ce8d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-a11y', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'cd16b283cad10202a8ce');
