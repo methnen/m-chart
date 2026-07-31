3fc8baa72d9024cc710cab736c5882dbf6c2b4e3
Jamie Poitra <jamie@methnen.com>
2026-07-31 14:03:58 -0700

    Version number bump.

--- m-chart.php
+++ m-chart.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: M Chart
-Version: 2.2.2
+Version: 2.3
 Plugin URI: https://github.com/methnen/m-chart
 Description: Manage data sets via a spreadsheet interface, display them as charts via the Chart.js chart library, and embed them via a shortcode or block.
 Author: Jamie Poitra
